30b7d0fa8f939e8c0b14a7900870e9c011648a93
Sérgio Barza <sergio.barza@celfocus.com>
2022-10-01 13:27:57 +0100

    Adde Primeiro Nome and Último Nome fields from Nome Completo field.

--- frontend/src/components/Forms/PersonalForm/PersonalForm.js
+++ frontend/src/components/Forms/PersonalForm/PersonalForm.js
@@ -16,57 +16,63 @@ const PersonalForm = () => {
     setRegion(val);
   };
 
+  const registerPersonalTrainerHandler = async () => {};
+
   return (
-    <>
-      <form onSubmit>
-        <div>
-          <label>Morada:*</label>
-          <input type="text" />
-        </div>
-        <div>
-          <label>País:*</label>
-          <CountryDropdown
-            defaultOptionLabel="--Selecione o País--"
-            value={country}
-            onChange={(val) => selectCountryHandler(val)}
-          />
-        </div>
-        <div>
-          <label>Estado/Região:*</label>
-          <RegionDropdown
-            defaultOptionLabel="--Selecione o Estado--"
-            disableWhenEmpty={true}
-            country={country}
-            value={region}
-            onChange={(val) => selectRegionHandler(val)}
-          />
-        </div>
-        <div>
-          <label>Cidade:*</label>
-          <input type="text" />
-        </div>
-        <div>
-          <label>Código Postal:*</label>
-          <input type="text" />
-        </div>
-        <div>
-          <label>Nome Completo:*</label>
-          <input type="text" />
-        </div>
-        <div>
-          <label>NIF Contribuinte:*</label>
-          <input type="text" />
-        </div>
-        <div>
-          <label>Email:*</label>
-          <input type="text" />
-        </div>
-        <Password label="Password:*" />
-        <Password label="Confirme Nova Password*" />
-        <Checkbox label="Li e concordo com os Termos e Condições*" />
-        <Checkbox label="A PT TRAINING recolhe dados informativos do usuário. Li e Concordo com a nossa Privacy Policy." />
-        <button type="button">Subscrever</button>
-      </form>
+    <form onSubmit={registerPersonalTrainerHandler}>
+      <div>
+        <label>Primeiro Nome:*</label>
+        <input type="text" />
+      </div>
+      <div>
+        <label>Último Nome:*</label>
+        <input type="text" />
+      </div>
+      <div>
+        <label>Morada:*</label>
+        <input type="text" />
+      </div>
+      <div>
+        <label>País:*</label>
+        <CountryDropdown
+          defaultOptionLabel="--Selecione o País--"
+          value={country}
+          onChange={(val) => selectCountryHandler(val)}
+        />
+      </div>
+      <div>
+        <label>Estado/Região:*</label>
+        <RegionDropdown
+          defaultOptionLabel="--Selecione o Estado--"
+          disableWhenEmpty={true}
+          country={country}
+          value={region}
+          onChange={(val) => selectRegionHandler(val)}
+        />
+      </div>
+      <div>
+        <label>Cidade:*</label>
+        <input type="text" />
+      </div>
+      <div>
+        <label>Código Postal:*</label>
+        <input type="text" />
+      </div>
+      <div>
+        <label>NIF Contribuinte:*</label>
+        <input type="text" />
+      </div>
+      <div>
+        <label>Email:*</label>
+        <input type="text" />
+      </div>
+      <Password label="Password:*" />
+      <Password label="Confirme Nova Password*" />
+      <Checkbox label="Li e concordo com os Termos e Condições*" />
+      <Checkbox label="A PT TRAINING recolhe dados informativos do usuário. Li e Concordo com a nossa Privacy Policy." />
+      <button type="button" onClick={registerPersonalTrainerHandler}>
+        Subscrever
+      </button>
       <p>
         Acesso válido após, o pagamento efectuado por transferência bancária ou
         depósito directo na nossa conta. IBAN: PT50 003300004564015394805, LILAS
@@ -74,7 +80,7 @@ const PersonalForm = () => {
         geral@pt-training.pt e indique o Primeiro e Ultimo Nome juntamente com o
         nome de utilizador/membro.
       </p>
-    </>
+    </form>
   );
 };
 
