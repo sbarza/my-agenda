0fcedb97cfa34819ae19c5b6fa929742e8b1a0ff
Filipe Torres <filipe_torres@apple.com>
2022-10-18 17:53:36 -0300

    Last basic structure and styles

--- frontend/src/components/SobreNos/SobreNos.js
+++ frontend/src/components/SobreNos/SobreNos.js
@@ -1,7 +1,8 @@
 import React from "react";
 import styles from "./SobreNos.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faStreetView, faUserCheck, faUsers } from "@fortawesome/free-solid-svg-icons";
+import { faClock, faEdit } from "@fortawesome/free-regular-svg-icons";
+import { faCalculator } from "@fortawesome/free-solid-svg-icons";
 import sobreNosImagem1 from "../../images/quem-somos__inicio.jpeg";
 import sobreNosImagem2 from "../../images/quem-somos__ginasios-e-estudios.jpeg";
 
@@ -9,14 +10,15 @@ const SobreNos = () => {
   return (
     <>
       <section className={styles["container-top-image"]}>
-        <a href="#como-funciona">
-          <img src={sobreNosImagem1} alt="Personal Trainer"/>
-          <h2 className={"title--alpha fadeInUp animated"}>SOBRE NÓS</h2>
-          <h3>OTIMIZA OS TEUS TREINOS, RENTABILIZA O TEU GINÁSIO</h3>
-        </a>
+        <img src={sobreNosImagem1} alt="Personal Trainer"/>
+        <h2 className={"title--alpha fadeInUp animated"}>SOBRE NÓS</h2>
       </section>
 
-      <section className={`container ${styles["como-funciona"]}`}>
+      <section className={styles["container-top-title"]}>
+        <h2>OTIMIZA OS TEUS TREINOS, RENTABILIZA O TEU GINÁSIO</h2>
+      </section>
+
+      <section className={`container ${styles["our-mission"]}`}>
         <h2>A NOSSA MISSÃO</h2>
         <p>Mais do que uma marca, a <strong>PT TRAINING </strong> apresenta-se como a plataforma digital 
           que liga os <em>ginásios</em> e os <em>personal trainers</em>, a <strong>PT TRAINING </strong> 
@@ -29,22 +31,37 @@ const SobreNos = () => {
           A <strong>PT TRAINING</strong>, apresenta a sua plataforma como uma ferramenta de 
           rentabilidade e otimização de recursos do <em>estúdio/ginásio</em>, oferecendo gratuitamente 
           um perfil a cada espaço para que possam servir todos os <em>personal trainers</em>.</p>
-          <div className={styles["como-funciona__item"]}>
-          <FontAwesomeIcon icon={faStreetView}/>
-          <h3 className={"title--beta"}>Descobrir</h3>
-          <p>Descobre na nossa plataforma as vantagens para um Ginásio/Estúdio ou um Personal Trainer.</p>
+          <div className={styles["our-mission__item"]}>
+          <FontAwesomeIcon icon={faClock}/>
+          <h3 className={"title--beta"}>Otimização</h3>
+          <p>Ajudamos a otimizar os treinos do Personal Trainer com agendamentos através da nossa plataforma.</p>
         </div>
-        <div className={styles["como-funciona__item"]}>
-          <FontAwesomeIcon icon={faUserCheck}/>
-          <h3 className={"title--beta"}>Registar</h3>
-          <p>Regista-te como Personal Trainer ou Ginásio/Estúdio de forma gratuita.</p>
+        <div className={styles["our-mission__item"]}>
+          <FontAwesomeIcon icon={faEdit}/>
+          <h3 className={"title--beta"}>Gestão</h3>
+          <p>Disponibilizamos numa só plataforma as ferramentas necessárias para otimizar os treinos e gestão 
+            do Personal Trainer.</p>
         </div>
-        <div className={styles["como-funciona__item"]}>
-          <FontAwesomeIcon icon={faUsers}/>
-          <h3 className={"title--beta"}>Acesso</h3>
-          <p>Tem acesso a todas as ferramentas e vantagens do PT Training 
-            enquanto Personal Trainer ou Ginásio/Estúdio.</p>
+        <div className={styles["our-mission__item"]}>
+          <FontAwesomeIcon icon={faCalculator}/>
+          <h3 className={"title--beta"}>Rentabilidade</h3>
+          <p>Torna-te parceiro e regista os teus Ginásios/Estúdios. Rentabiliza o teu espaço e acessórios 
+            com a <strong>PT TRAINING</strong>.</p>
         </div>
+      </section>
+
+      <section className={styles["container-bottom-image"]}>
+        <div className={styles["container-bottom-image__wrapper"]}>
+          <img src={sobreNosImagem2} alt="Ginásio"/>
+        </div>
+        <h2 className={styles["container-bottom-image__title-top"]}>
+          <span>Performance</span><br/>
+          NÓS AJUDAMOS-TE NA OTIMIZAÇÃO DE RESULTADOS
+        </h2>
+        <h2 className={styles["container-bottom-image__title-middle"]}>
+          <span>Comunidade</span><br/>
+          <strong>PT TRAINING</strong> O TEU PARCEIRO NO FITNESS
+        </h2>
       </section>
 
       <section className={styles["fica-atento"]}>
