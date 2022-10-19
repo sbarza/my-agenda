2622e90f895d363f2bcd47e11f1caeda0e58173a
Filipe Torres <filipe_torres@apple.com>
2022-10-18 23:32:57 -0300

    Added Newsletter form component

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -8,6 +8,7 @@ import homeImageThird from "../../images/home-third.jpeg";
 import homeImageInstagramUser from "../../images/home-rodrigogodinho.jpeg";
 import homeImageForth1 from "../../images/home-forth-1.jpeg";
 import homeImageForth2 from "../../images/home-forth-2.jpeg";
+import Newsletter from "../Forms/Newsletter/Newsletter";
 
 const Home = () => {
   React.useEffect(() => {
@@ -162,45 +163,7 @@ const Home = () => {
         </div>
       </section>
 
-      <section className={styles["fica-atento"]}>
-        <div className={`container`}>
-          <h2>Fica Atento!</h2>
-          <h3>Com a nossa newsletter semanal não perderás nenhuma novidade do PT TRAINING.</h3>
-          <input type="text" placeholder="Nome"/>
-          <input type="text" placeholder="O teu melhor email"/>
-          <select name="form_fields[message]" id="form-field-message">
-            <option value="Aveiro">Aveiro</option>
-            <option value="Beja">Beja</option>
-            <option value="Braga">Braga</option>
-            <option value="Bragança">Bragança</option>
-            <option value="Castelo Branco">Castelo Branco</option>
-            <option value="Coimbra">Coimbra</option>
-            <option value="Évora">Évora</option>
-            <option value="Faro">Faro</option>
-            <option value="Guarda">Guarda</option>
-            <option value="Leiria">Leiria</option>
-            <option value="Lisboa">Lisboa</option>
-            <option value="Porto">Porto</option>
-            <option value="Portalegre">Portalegre</option>
-            <option value="Santarém">Santarém</option>
-            <option value="Setúbal">Setúbal</option>
-            <option value="Vila Real">Vila Real</option>
-            <option value="Viseu">Viseu</option>
-            <option value="Viana do Castelo">Viana do Castelo</option>
-          </select>
-          <div>
-            <input type="radio" value="Ginásio/Estúdio" id="fica-atento-form__ginasio-estudio" name="fica-atento-form__ginasio-estudio"/>
-            <label htmlFor="fica-atento-form__ginasio-estudio">Ginásio/Estúdio</label>
-            <input type="radio" value="Personal Trainer" id="fica-atento-form__personal-trainer" name="fica-atento-form__personal-trainer"/>
-            <label htmlFor="fica-atento-form__personal-trainer">Personal Trainer</label>
-          </div>
-          <div>
-            <input type="checkbox" value="Li e Aceito a Politica de Privacidade." id="fica-atento-form__politica-privacidade" name="fica-atento-form__politica-privacidade"></input>
-            <label htmlFor="fica-atento-form__politica-privacidade">Li e Aceito a Politica de Privacidade.</label>
-          </div>
-          <button type="submit">SUBSCREVER</button>
-        </div>
-      </section>
+      <Newsletter />
     </>
   );
 };
--- frontend/src/components/SobreNos/SobreNos.js
+++ frontend/src/components/SobreNos/SobreNos.js
@@ -5,6 +5,7 @@ import { faClock, faEdit } from "@fortawesome/free-regular-svg-icons";
 import { faCalculator } from "@fortawesome/free-solid-svg-icons";
 import sobreNosImagem1 from "../../images/quem-somos__inicio.jpeg";
 import sobreNosImagem2 from "../../images/quem-somos__ginasios-e-estudios.jpeg";
+import Newsletter from "../Forms/Newsletter/Newsletter";
 
 const SobreNos = () => {
   return (
@@ -64,45 +65,7 @@ const SobreNos = () => {
         </h2>
       </section>
 
-      <section className={styles["fica-atento"]}>
-        <div className={`container`}>
-          <h2>Fica Atento!</h2>
-          <h3>Com a nossa newsletter semanal não perderás nenhuma novidade do PT TRAINING.</h3>
-          <input type="text" placeholder="Nome"/>
-          <input type="text" placeholder="O teu melhor email"/>
-          <select name="form_fields[message]" id="form-field-message">
-            <option value="Aveiro">Aveiro</option>
-            <option value="Beja">Beja</option>
-            <option value="Braga">Braga</option>
-            <option value="Bragança">Bragança</option>
-            <option value="Castelo Branco">Castelo Branco</option>
-            <option value="Coimbra">Coimbra</option>
-            <option value="Évora">Évora</option>
-            <option value="Faro">Faro</option>
-            <option value="Guarda">Guarda</option>
-            <option value="Leiria">Leiria</option>
-            <option value="Lisboa">Lisboa</option>
-            <option value="Porto">Porto</option>
-            <option value="Portalegre">Portalegre</option>
-            <option value="Santarém">Santarém</option>
-            <option value="Setúbal">Setúbal</option>
-            <option value="Vila Real">Vila Real</option>
-            <option value="Viseu">Viseu</option>
-            <option value="Viana do Castelo">Viana do Castelo</option>
-          </select>
-          <div>
-            <input type="radio" value="Ginásio/Estúdio" id="fica-atento-form__ginasio-estudio" name="fica-atento-form__ginasio-estudio"/>
-            <label htmlFor="fica-atento-form__ginasio-estudio">Ginásio/Estúdio</label>
-            <input type="radio" value="Personal Trainer" id="fica-atento-form__personal-trainer" name="fica-atento-form__personal-trainer"/>
-            <label htmlFor="fica-atento-form__personal-trainer">Personal Trainer</label>
-          </div>
-          <div>
-            <input type="checkbox" value="Li e Aceito a Politica de Privacidade." id="fica-atento-form__politica-privacidade" name="fica-atento-form__politica-privacidade"></input>
-            <label htmlFor="fica-atento-form__politica-privacidade">Li e Aceito a Politica de Privacidade.</label>
-          </div>
-          <button type="submit">SUBSCREVER</button>
-        </div>
-      </section>
+      <Newsletter />
     </>
   );
 };
